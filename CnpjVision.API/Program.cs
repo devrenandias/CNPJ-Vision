6d24b0720e32f028c06c7f47d77f88f4fee5a74c
Renan Dias <renandiasdev@gmail.com>
2025-07-29 09:29:31 -0300

    Update Program.cs

--- CnpjVision.API/Program.cs
+++ CnpjVision.API/Program.cs
@@ -6,22 +6,19 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApiDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"),
+        npgsqlOptions => npgsqlOptions.CommandTimeout(60)) // aumenta o timeout
 );
 
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<EmpresaService>();
 
 builder.Services.AddHttpClient();
-
 
 builder.Services.AddAuthentication(options =>
 {
@@ -36,28 +33,25 @@ builder.Services.AddAuthentication(options =>
         ValidateAudience = true,
         ValidateLifetime = true,
         ValidateIssuerSigningKey = true,
-
         ValidIssuer = builder.Configuration["Jwt:Issuer"],
         ValidAudience = builder.Configuration["Jwt:Audience"],
         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
-
-        ClockSkew = TimeSpan.Zero 
+        ClockSkew = TimeSpan.Zero
     };
 });
 
+// CORS para frontend do Vercel
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("cnpjVisionApp", builder =>
+    options.AddPolicy("AllowVercel", builder =>
     {
-       builder.WithOrigins("http://localhost:4200", "https://cnpj-vision.vercel.app")
-      .AllowAnyHeader()
-      .AllowAnyMethod()
-      .AllowCredentials();
+        builder.WithOrigins("https://cnpj-vision.vercel.app")
+               .AllowAnyHeader()
+               .AllowAnyMethod();
     });
 });
 
 var app = builder.Build();
-
 
 if (app.Environment.IsDevelopment())
 {
@@ -66,13 +60,15 @@ if (app.Environment.IsDevelopment())
 }
 
 
-app.UseCors("cnpjVisionApp");
+app.UseCors("AllowVercel");
 
 app.UseHttpsRedirection();
-
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+
+app.MapGet("/health", () => Results.Ok("OK"));
 
 app.Run();
