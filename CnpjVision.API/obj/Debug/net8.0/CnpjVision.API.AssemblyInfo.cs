0647a57ecb868c7f0a2b034da7d3000a0914d886
Renan Dias <renandiasdev@gmail.com>
2025-07-28 22:11:55 -0300

    Integração SUPABASE

--- CnpjVision.API/obj/Debug/net8.0/CnpjVision.API.AssemblyInfo.cs
+++ CnpjVision.API/obj/Debug/net8.0/CnpjVision.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CnpjVision.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0a12dc509540601c1c22aaa61c67201bb36fa1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6922c33e597078bfaea9c6a6715e42c1adf408bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("CnpjVision.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CnpjVision.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
